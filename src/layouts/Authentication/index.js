d536c2a5f466f27988346b27cb7fc3d7cafb00f2
minhhung123 <prohungnguyenminh@gmail.com>
2020-12-09 00:13:14 +0700

    Initial auth page

---
+++ src/layouts/Authentication/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Card } from "antd";
+import "antd/dist/antd.css";
+import { SignInForm } from "../../modules/Forms/SignInForm";
+import { SignUpForm } from "../../modules/Forms/SignUpForm";
+
+const tabList = [
+  {
+    key: 'Sign In',
+    tab: 'Sign In',
+  },
+  {
+    key: 'Sign Up',
+    tab: 'Sign Up',
+  },
+];
+
+const AuthLayout = () => {
+  const [key, setKey] = useState('Sign In');
+  const onTabChange = (key) => {
+    setKey(key);
+  };
+  return (
+    <div>
+      <img src="/logo.jpeg" alt="" />
+      <Card
+          style={{ width: '100%' }}
+          title={key}
+          tabList={tabList}
+          activeTabKey={key}
+          onTabChange={key => {
+            onTabChange(key, 'key');
+          }}
+        >
+          {key === 'Sign In' ? <SignInForm /> : <SignUpForm/>}
+        </Card>
+    </div>
+  );
+};
+
+export default AuthLayout;
